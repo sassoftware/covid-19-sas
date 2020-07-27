fc1725b998530d2aeccc014b582d6b69a2554aa0
Bozidar Zecevic bozazec@gmail.com <bozazec@gmail.com>
2020-07-27 13:42:18 +0200

    Improve range slider on input chart

--- App/src/pages/home/home.js
+++ App/src/pages/home/home.js
@@ -5,7 +5,7 @@ import {Content} from "carbon-components-react/lib/components/UIShell";
 
 const Home = () => (<Content id='main-content'>
 		<div className={'home'}>
-			<h1 className={'lyb3'}>CoV-19 App</h1>
+			<h1 className={'lyb3'}>Hospital Scenario Analysis</h1>
 			<h4 className={'spb3'}>A CSA PWA front end to <span
 				className={'text-bold'}>github.com/sassoftware/covid-19-sas</span></h4>
 			<p>This page is a placeholder for instructions and documentation.</p>
